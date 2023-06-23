82be7a25c02cda722c4ce22c2d3fd4f26a503ecc
Kashiwade-music <music5d7_9265@outlook.jp>
2023-06-23 09:16:40 +0900

    update cq

--- cq_changer.ps1
+++ cq_changer.ps1
@@ -1,15 +1,14 @@
 # CQ = x12.8
 # CRF = x10.2
+$cq = 32
 
 foreach($files in $(Get-ChildItem -Recurse)) {
-    if (-not ($files.BaseName -match "^CRF_28_")) {
-        if (-not ($files.BaseName -match "^CQ_35_")){
+        if (-not ($files.BaseName -match "^CRF_") -and -not ($files.BaseName -match "^CQ_")){ {
             if (($($files.Extension) -eq ".mkv") -or ($($files.Extension) -eq ".mp4")) {
                 Write-Host $files.FullName
                 $sum=$sum+$files.Length
             }
         }
-        
     }
 }
 
@@ -29,13 +28,11 @@ $answer = $host.ui.PromptForChoice("<実行確認>","実行しますか？",$cho
 
 if ($answer -eq 0){
     foreach($files in $(Get-ChildItem -Recurse)){
-        if (-not ($files.BaseName -match "^CRF_28_")) {
-            if (-not ($files.BaseName -match "^CQ_35_")){
-                if ($($files.Extension) -eq ".mkv" -or $($files.Extension) -eq ".mp4") {
-                    $newName = "CQ_35_" + $files.BaseName + ".mp4"
-                    # Write-Host $newName
-                    ffmpeg -i $files.FullName -c:v h264_nvenc -cq 35 $newName
-                }
+        if (-not ($files.BaseName -match "^CRF_") -and -not ($files.BaseName -match "^CQ_")) {
+            if ($($files.Extension) -eq ".mkv" -or $($files.Extension) -eq ".mp4") {
+                $newName = "CQ_" + $cq + "_" + $files.BaseName + ".mp4"
+                # Write-Host $newName
+                ffmpeg -i $files.FullName -c:v h264_nvenc -cq $cq $newName
             }
         }
     }
