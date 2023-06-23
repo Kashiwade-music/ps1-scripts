e73902a4e486c409d62b71cef558eceb4808783a
Kashiwade-music <music5d7_9265@outlook.jp>
2023-06-23 09:40:01 +0900

    update

--- crf_changer.ps1
+++ crf_changer.ps1
@@ -1,4 +1,4 @@
-
+$crf = 28
 
 foreach($files in $(Get-ChildItem -Recurse)) {
     if (-not ($files.BaseName -match "^CRF_") -and -not ($files.BaseName -match "^CQ_")) {
@@ -27,9 +27,9 @@ if ($answer -eq 0){
     foreach($files in $(Get-ChildItem -Recurse)){
         if (-not ($files.BaseName -match "^CRF_") -and -not ($files.BaseName -match "^CQ_")) {
             if ($($files.Extension) -eq ".mkv" -or $($files.Extension) -eq ".mp4") {
-                $newName = "CRF_28_" + $files.BaseName + ".mp4"
+                $newName = "CRF_" + $crf + "_" + $files.BaseName + ".mp4"
                 # Write-Host $newName
-                ffmpeg -i $files.FullName -crf 28 $newName
+                ffmpeg -i $files.FullName -crf $crf $newName
             }
         }
     }
