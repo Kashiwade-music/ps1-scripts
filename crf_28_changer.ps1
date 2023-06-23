9776ea1b04ce6aaa1a50817540ecbe4e7cbf8509
Kashiwade-music <music5d7_9265@outlook.jp>
2023-06-23 09:37:40 +0900

    update

--- crf_28_changer.ps1
+++ crf_28_changer.ps1
@@ -1,14 +1,11 @@
 
 
 foreach($files in $(Get-ChildItem -Recurse)) {
-    if (-not ($files.BaseName -match "^CRF_28_")) {
-        if (-not ($files.BaseName -match "^CQ_35_")){
-            if (($($files.Extension) -eq ".mkv") -or ($($files.Extension) -eq ".mp4")) {
-                Write-Host $files.FullName
-                $sum=$sum+$files.Length
-            }
+    if (-not ($files.BaseName -match "^CRF_") -and -not ($files.BaseName -match "^CQ_")) {
+        if (($($files.Extension) -eq ".mkv") -or ($($files.Extension) -eq ".mp4")) {
+            Write-Host $files.FullName
+            $sum=$sum+$files.Length
         }
-        
     }
 }
 
@@ -28,13 +25,11 @@ $answer = $host.ui.PromptForChoice("<実行確認>","実行しますか？",$cho
 
 if ($answer -eq 0){
     foreach($files in $(Get-ChildItem -Recurse)){
-        if (-not ($files.BaseName -match "^CRF_28_")) {
-            if (-not ($files.BaseName -match "^CQ_35_")){
-                if ($($files.Extension) -eq ".mkv" -or $($files.Extension) -eq ".mp4") {
-                    $newName = "CRF_28_" + $files.BaseName + ".mp4"
-                    # Write-Host $newName
-                    ffmpeg -i $files.FullName -crf 28 $newName
-                }
+        if (-not ($files.BaseName -match "^CRF_") -and -not ($files.BaseName -match "^CQ_")) {
+            if ($($files.Extension) -eq ".mkv" -or $($files.Extension) -eq ".mp4") {
+                $newName = "CRF_28_" + $files.BaseName + ".mp4"
+                # Write-Host $newName
+                ffmpeg -i $files.FullName -crf 28 $newName
             }
         }
     }
